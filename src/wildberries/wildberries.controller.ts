028a7f5e671863d88c70b6c223eac8157aee2d31
Evgeniy Syrov <surovjeniya@gmail.com>
2023-08-01 09:18:31 +0300

    ...

--- src/wildberries/wildberries.controller.ts
+++ src/wildberries/wildberries.controller.ts
@@ -1,4 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+} from '@nestjs/common';
 import { WildberriesService } from './wildberries.service';
 
 @Controller('wildberries')
@@ -7,34 +12,57 @@ export class WildberriesController {
 
   @Post('send-phone-number')
   async sendPhoneNumber(@Body() dto: any) {
-    return await this.wildberriesService.sendPhoneNumber(dto.phone_number);
+    try {
+      return await this.wildberriesService.sendPhoneNumber(dto.phone_number);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   @Post('send-code')
   async sendCode(@Body() dto: any) {
-    return await this.wildberriesService.sendCode(dto.code);
+    try {
+      return await this.wildberriesService.sendCode(dto.code);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   @Post('change-shop')
   async changeShop(@Body() dto: any) {
-    return await this.wildberriesService.changeShop(dto.shop_id, dto.shop_name);
+    try {
+      return await this.wildberriesService.changeShop(
+        dto.shop_id,
+        dto.shop_name,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   @Post('download-fourteen-order')
   async downloadFourteenOrder(@Body() dto: any) {
-    return await this.wildberriesService.downloadFourteenOrder(
-      dto.shop_name,
-      dto.start_date,
-      dto.end_date,
-    );
+    try {
+      return await this.wildberriesService.downloadFourteenOrder(
+        dto.shop_name,
+        dto.start_date,
+        dto.end_date,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 
   @Post('go-to-analytics')
   async goToAnalytics(@Body() dto: any) {
-    return await this.wildberriesService.gotoFourteenOrder(
-      dto.shop_name,
-      dto.start_date,
-      dto.end_date,
-    );
+    try {
+      return await this.wildberriesService.gotoFourteenOrder(
+        dto.shop_name,
+        dto.start_date,
+        dto.end_date,
+      );
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
   }
 }
